9f482be6791f845944e6da96a571de4637ee7a6c
TagawaHirotaka <mariobaske@i.softbank.jp>
2020-07-18 15:31:27 +0900

    Update 4-2-4.ts

--- 4-2/4-2-4.ts
+++ 4-2/4-2-4.ts
@@ -7,6 +7,7 @@ greet() // RunTime Error (Non-null assertion)
 function greet2(name?: string) {
     console.log(`Hello ${name!.toLocaleUpperCase}`)
 }
+// !(Non-null assertion)は「nullおよびundefinedではないという」状況が余程信頼できない限り利用すべきではない
 
 greet2() // Hello undefined
 
